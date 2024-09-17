306a81e37cd49759e7bd47cbb1c8d43b019d7b5f
Victor <Victorsancheznogueira@gmail.com>
2024-09-17 19:17:43 +0200

    Ej5

--- .idea/sonarlint/securityhotspotstore/index.pb
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -5,3 +5,19 @@ R
 "src/main/java/edu/badpals/Ej3.java,6\b\6b973687a3c4abd2b6a5b8f164e637874e83ff7f
 ;
 ejemplo.txt,f\1\f1cf2a2cef747721c5ce89eb0f6de904b2989ce8
+R
+"src/main/java/edu/badpals/Ej5.java,c\2\c2981bbfc38bfd31aaff2a9124df330d682a27f8
+:
+
+coches.txt,c\e\ce0cca1c82b327f6aa7c9576fddba7195a089090
+\
+,src/main/java/edu/badpals/EJ4_Corrector.java,4\5\45dfcd2fecb03f6ecd991b11111e03cf788e75b6
+R
+"src/main/java/edu/badpals/EJ4.java,9\0\90ef3e70e9d8c522a067800b8aa8575b06691e79
+:
+
+marcas.txt,e\7\e7697ad07f5f056148bee7691b75e02e9194bae4
+]
+-.idea/sonarlint/securityhotspotstore/index.pb,8\8\88a9255124c95bdc913197c120a8d560edc59c8e
+S
+#.idea/sonarlint/issuestore/index.pb,9\f\9fe84ebb15faf917b7def6236dba604453cc61e0
---
+++ src/main/java/edu/badpals/Ej5.java
@@ -0,0 +1,48 @@
+package edu.badpals;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.nio.file.Path;
+import java.util.*;
+
+public class Ej5 {
+    public static void main(String[] args){
+        String path = "./coches.txt";
+        Map<String, String> dicc = Ej5.leectorCoches(path);
+        System.out.println(Ej5.leectorCoches(path));
+        escitorCoches(dicc,"./marcas.txt");
+    }
+
+    private static Map<String, String> leectorCoches(String p) {
+        Path path = Path.of(p);
+        Map<String, String > dicc = new HashMap<>();
+        try(var lector = new Scanner(path)){
+            while (lector.hasNextLine()){
+                String linea = lector.nextLine();
+                if (linea.isBlank()){
+                    continue;
+                }
+                int index = linea.indexOf(" ");
+                String marca = linea.substring(0,index);
+                String modelo = linea.substring(index+1);
+                dicc.computeIfPresent(marca,(k,v) -> v + ", " + modelo);
+                dicc.putIfAbsent(marca,modelo);
+            }
+            return dicc;
+        } catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+        return dicc;
+    }
+
+    private static void escitorCoches(Map<String, String> dicc,String path){
+        try(BufferedWriter escritor = new BufferedWriter(new FileWriter(path,false))){
+            for (Map.Entry<String, String> entry : dicc.entrySet()){
+                escritor.write(entry.toString().replace("=",": "));
+                escritor.newLine();
+            }
+        } catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
