0c58a8f7969d78b1831f7131acb8a396525e528f
Victor <victorsancheznogueira@gmail.com>
2024-09-20 11:10:55 +0200

    Ej2_Extra

--- src/main/java/edu/badpals/Ej2_Extra.java
+++ src/main/java/edu/badpals/Ej2_Extra.java
@@ -1,45 +1,80 @@
 package edu.badpals;
 
-import java.util.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
 
 public class Ej2_Extra {
 
-    private Scanner sc = new Scanner(System.in);
+    private final Scanner sc = new Scanner(System.in);
     private String respuesta = "";
-    private final Set<String> RESPUESTAS = new HashSet<>(Arrays.asList("cat","cd","ls"));
     private String path = System.getProperty("user.dir");
-    private final StringBuilder INTERFAZ_MENU = new StringBuilder("Que quieres hacer:\n-cd cambiar de directorio\n-ls mostrar el contenido del directorio actual\n-cat mostrar el contenido de un archivo\n-exit salir del menu\n").append("\n>").append(path).append(" ");
+    private final StringBuilder INTERFAZ_MENU = new StringBuilder("Que quieres hacer:\n-cd cambiar de directorio\n-ls mostrar el contenido del directorio actual\n-cat mostrar el contenido de un archivo\n-exit salir del menu\n");
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Ej2_Extra app = new Ej2_Extra();
         app.openMenu();
     }
 
     private void openMenu() {
-        while (!respuesta.equals("exit")){
+        while (!respuesta.equals("exit")) {
             System.out.println(INTERFAZ_MENU);
+            System.out.println(">" + this.path);
             respuesta = sc.nextLine();
-            if (!RESPUESTAS.contains(respuesta)){
-                System.out.println("Esa no es una respuesta valida");
-                this.openMenu();
-            }
-            if (respuesta.equals("ls")){
-                ls(path);
-            } else if (respuesta.equals("cat")) {
-                cat(path);
+            switch (respuesta) {
+                case "ls" -> ls(this.path);
+                case "cat" -> cat(this.path);
+                case "cd" -> cd(this.path);
+                default -> {
+                    if (respuesta.equals("exit")) {
+                        System.out.println("Saliendo de la terminal");
+                    } else {
+                        System.out.println("Esa no es una respuesta valida");
+                    }
+                }
             }
         }
     }
 
-    private static void ls(String path) {
+    private void ls(String path) {
         System.out.println(Ej1_Extra.arbolDirectorios(path));
     }
 
-    private void cat(String path){
-        System.out.println("Que archivo quieres leer");
-        String archivo = sc.nextLine();
-        System.out.println(Ej3.lectorDeFichero(archivo));
+    private void cd(String dondeEstamos) {
+        System.out.println("A que direccion quieres ir");
+        String dondeVamos = sc.nextLine();
+        String stringResultante;
+        if (Path.of(dondeVamos).isAbsolute()) {
+            stringResultante = dondeVamos;
+        } else {
+            while (dondeVamos.startsWith("..\\") || dondeVamos.startsWith("../")) {
+                dondeEstamos = Path.of(dondeEstamos).getParent().toString();
+                dondeVamos = dondeVamos.substring(3);
+            }
+            stringResultante = Paths.get(dondeEstamos, dondeVamos).normalize().toString();
+        }
+        Path pathResultante = Path.of(stringResultante);
+        if (Files.isDirectory(pathResultante)) {
+            this.path = stringResultante;
+        } else {
+            if (Files.exists(pathResultante)) {
+                System.out.println("no puedes ir a un archivo");
+            } else {
+                System.out.println(pathResultante);
+                System.out.println("Esta ruta no existe");
+            }
+        }
+
     }
 
+    private void cat(String path) {
+        System.out.println("Que archivo quieres leer");
+        String pathArchivo = Paths.get(path, sc.nextLine()).normalize().toString();
+        for (String linea : Ej3.lectorDeFichero(pathArchivo)) {
+            System.out.println(linea);
+
+        }
+    }
 
 }
